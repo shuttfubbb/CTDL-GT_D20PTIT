96fcc8570fb4995372ff771c071a55c728b47458
ngquanghuy <70921465+huytre123qwe@users.noreply.github.com>
2022-03-03 22:38:37 +0700

    Add new files

---
+++ Bai_tap_CTDL&GT/DSA09001.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while(t--)
+    {
+        int n, k;
+        vector<vector<int> > dothi;
+        cin >> n >> k;
+        for(int i=0; i<n; ++i)
+        {
+            vector<int> a;
+            dothi.push_back(a);
+        }
+        while(k--)
+        {
+            
+        }
+    }
+    return 0;
+}
